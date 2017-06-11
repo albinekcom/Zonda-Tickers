b4464ed089e092a7146ef624225324935a81e6e5
Albin "albinek" Sadowski <albinek@albinek.com>
2017-06-11 18:48:47 +0200

    Add basic files in Tests directory

---
+++ Tests/BitBayTickerTests/TickerTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+
+final class TickerTests: XCTestCase {
+
+    func testTrue() {
+        XCTAssertTrue(true)
+    }
+
+
+    static var allTests = [
+        ("testTrue", testTrue)
+    ]
+
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import BitBayTickerTests
+
+XCTMain([
+    testCase(TickerTests.allTests),
+])
