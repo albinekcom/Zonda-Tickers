8ca640ef262668398616924730ac21716dbf3563
Albin "albinek" Sadowski <albinek@albinek.com>
2017-07-29 13:31:49 +0200

    Improve TickerFactory

--- Sources/BitBay-TickerCore/TickerFactory.swift
+++ Sources/BitBay-TickerCore/TickerFactory.swift
@@ -3,12 +3,12 @@ import Foundation
 public struct TickerFactory {
     
     public static func makeTicker(named tickerName: String) -> Ticker? {
-        guard TickerNameValidator().isValid(name: tickerName) else { return nil }
-        
-        guard let url = URL(string: "https://bitbay.net/API/Public/\(tickerName)/ticker.json") else { return nil }
-        guard let jsonData = try? Data(contentsOf: url) else { return nil }
-        guard let jsonObject = try? JSONSerialization.jsonObject(with: jsonData) else { return nil }
-        guard let jsonDictionary = jsonObject as? [String: Any] else { return nil }
+        guard
+            TickerNameValidator().isValid(name: tickerName),
+            let url = URL(string: "https://bitbay.net/API/Public/\(tickerName)/ticker.json"),
+            let jsonData = try? Data(contentsOf: url),
+            let jsonObject = try? JSONSerialization.jsonObject(with: jsonData),
+            let jsonDictionary = jsonObject as? [String: Any] else { return nil }
         
         return Ticker(named: tickerName, jsonDictionary: jsonDictionary)
     }
