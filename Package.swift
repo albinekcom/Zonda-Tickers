4d2ba4fbf380c83945165739f8c47475d3cc5b23
Albin "albinek" Sadowski <albinek@albinek.com>
2018-07-07 20:49:59 +0200

    Improve Travis CI file

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "BitBay-Ticker",
 
     targets: [
+        .target(name: "BitBay-TickerCore"),
         .target(name: "BitBay-Ticker", dependencies: ["BitBay-TickerCore"]),
-        .target(name: "BitBay-TickerCore")
+        .testTarget(name: "BitBay-TickerCoreTests", dependencies: ["BitBay-TickerCore"], path: "Tests")
     ]
 )
